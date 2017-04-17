8ee2c083ab29f6e164014a87a84208ff5cb11f13
Luiz Gavioli <luizbag@gmail.com>
2017-04-17 13:48:03 -0300

    Adding the posts factory and calling it.

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,14 +1,11 @@
 var app = angular.module('app');
 
-app.controller('FeedController', function() {
+app.controller('FeedController', ['Post', function(Post) {
 	var ctrl = this;
 
-	ctrl.posts = [
-		{title: 'Post 1',
-		n_comments: 0,
-		url: 'http://google.com'},
-		{title: 'Post 2',
-		n_comments: 18,
-		url: 'http://google.com'}
-	];
-});
+	var init = function() {
+		ctrl.posts = Post.query();
+	};
+
+	init();
+}]);
--- public/js/services.js
+++ public/js/services.js
@@ -1,1 +1,5 @@
 var app = angular.module('app');
+
+app.factory('Post', ['$resource', function($resource) {
+    return $resource('/posts/:id');
+}]);
